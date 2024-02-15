821283006bfbcdbc8ec2650d5dbf4552b8432713
MarkoCutura56 <mcutura12@gmail.com>
2024-02-15 08:05:10 -0500

    Budget app preparation 1

--- src/App.js
+++ src/App.js
@@ -15,27 +15,45 @@ const App = () => {
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
                     <div className='row mt-3'>
-                        {
+                     {
+                        <div className='col-sm'>
+                        <Budget />
+                        </div>
                             /* Add Budget component here */
-                        }        
+                        }                                     
 
-                        {
+                     {
+                            <div className='col-sm'>
+                            <Remaining />
+                            </div>
                             /* Add Remaining component here*/
                         }        
 
-                        {
+                     {
+                            <div className='col-sm'>
+                            <ExpenseTotal />
+                            </div>
                             /* Add ExpenseTotal component here */
                         }        
                        
-                        {
+                     {
+                            <div className='col-sm'>
+                            <ExpenseList />
+                            </div>
                             /* Add ExpenseList component here */
                         }         
 
-                        {
+                     {
+                            <div className='col-sm'>
+                            <ExpenseItem />
+                            </div>
                             /* Add ExpenseItem component here */
                         }        
 
-                        {
+                     {
+                            <div className='col-sm'>
+                            <AllocationForm />
+                            </div>
                             /* Add AllocationForm component here under */
                         }        
 
