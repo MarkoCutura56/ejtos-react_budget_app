a141af860b66559ef5f974f3e322dec166e315f2
MarkoCutura56 <mcutura12@gmail.com>
2024-02-20 09:44:15 -0500

    uzas

--- src/components/CurrencyChange.js
+++ src/components/CurrencyChange.js
@@ -4,10 +4,56 @@ import styles from './Currencystyle.module.css';
 const CurrencyChange = () =>
 {
     const currencyOptions = ['$ Dollar','£ Pound','€ Euro','₹ Ruppee']
-    const currencyOptionsDefault = ['$ Dollar','£ Pound','€ Euro','₹ Ruppee']
+    var currencyOptionsMod = ['$ Dollar','£ Pound','€ Euro','₹ Ruppee']
     const { dispatch } = useContext(AppContext);
     const { currency } = useContext(AppContext);
     
+    
+    const currencyStyle=
+    {
+        width : "300px",
+        color : "black",
+        
+        backgroundColor :  "Aquamarine",
+        padding :  "10x",
+        //border :  "1px solid",
+        borderRadius :  "5px" 
+    }
+    const currencyStyleleft=
+    {
+        
+        color : "black",
+        float :"left",
+        width : "40%",
+        backgroundColor :  "Aquamarine",
+        //padding :  "10x",
+        //border :  "1px solid",
+        //borderRadius :  "5px" 
+    }
+    const currencyStyleright=
+    {
+       
+        color : "black",
+        float :"right",
+        width : "60%",
+        backgroundColor :  "Aquamarine",
+        //padding :  "10x",
+        //border :  "1px solid",
+        //borderRadius :  "5px" 
+    }
+    const currencyStyleselected=
+    {
+       
+        color : "black",
+        position: "absolute",
+        left:"0",
+        right:"0",
+        width : "300px",
+        backgroundColor :  "Aquamarine",
+        //padding :  "10x",
+        //border :  "1px solid",
+        //borderRadius :  "5px" 
+    }
     const handleCurrencyChange = (event) =>
     {
         dispatch({type :'CHG_CURRENCY',payload: event.target.value,});
@@ -18,25 +64,25 @@ const CurrencyChange = () =>
             let usedElement = document.getElementbyID("inputGroupSelect012").selectedIndex;
             if(i==usedElement)
             {
-                currencyOptions[i]= "Currency:"+currencyOptions[i];  
+                currencyOptionsMod[i]= "Currency:"+currencyOptions[i];  
             }
             else
             {
-                currencyOptions[i]=currencyOptionsDefault[i]+"def";
+                currencyOptionsMod[i]=currencyOptions[i]+"def";
             }
         }
     }
     return(
     
-       <div className={styles.Currencystyle1}>
-       <label className={styles.Currencystyle1} for="inputGroupSelect012" >Currency:  </label>
-       <select className={styles.Currencystyle1} id="inputGroupSelect012" onChange={handleCurrencyChange}>
+       <div style={currencyStyle}>
+       <label style={currencyStyleleft} for="inputGroupSelect012" >Currency:  </label>
+       <select style={currencyStyleright} id="inputGroupSelect012" onChange={handleCurrencyChange}>
                 
-                <option defaultValue>{currency}</option>
-                <option value={currencyOptions[0]} name={currencyOptions[0]}>{currencyOptions[0]}</option>
-                <option value={currencyOptions[1] } name={currencyOptions[1]} selected>{currencyOptions[1]} </option>
-                <option value={currencyOptions[2]} name={currencyOptions[2]}>{currencyOptions[2]} </option>
-                <option value={currencyOptions[3]} name={currencyOptions[3]}>{currencyOptions[3]}</option>
+                <option style={currencyStyleselected} defaultValue>{currency+"car"}</option>
+                <option style={currencyStyleselected} value={currencyOptions[0]} name={currencyOptions[0]}>{currencyOptions[0]}</option>
+                <option style={currencyStyleselected} value={currencyOptions[1] } name={currencyOptions[1]} selected>{currencyOptions[1]} </option>
+                <option style={currencyStyleselected} value={currencyOptions[2]} name={currencyOptions[2]}>{currencyOptions[2]} </option>
+                <option style={currencyStyleselected} value={currencyOptions[3]} name={currencyOptions[3]}>{currencyOptions[3]}</option>
                   </select>
        </div>
     );
